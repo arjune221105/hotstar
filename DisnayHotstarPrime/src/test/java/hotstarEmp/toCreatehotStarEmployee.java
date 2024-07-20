13ca5420722b700bffd14392acd92ef8c384d432
arjune221105 <shakti221105@gmail.com>
2024-07-19 21:26:36 -0700

    pust

---
+++ DisnayHotstarPrime/src/test/java/hotstarEmp/toCreatehotStarEmployee.java
@@ -0,0 +1,10 @@
+package hotstarEmp;
+
+public class toCreatehotStarEmployee {
+	public static void main(String[] args) {
+		System.out.println("Disnay+Hotstar Marge App");
+	
+		System.out.println("All App Working fine");
+	}
+
+}
